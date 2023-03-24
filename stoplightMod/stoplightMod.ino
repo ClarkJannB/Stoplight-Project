1092c79c565c66be3fa3ced2da94f28f940b2095
Clark Jann Barayuga <92810969+ClarkJannB@users.noreply.github.com>
2023-03-24 13:20:41 -0400

    Add Cleanup to Array
    Added new section (warning time) to the array. Still in quick testing mode.

--- stoplightMod/stoplightMod.ino
+++ stoplightMod/stoplightMod.ino
@@ -1,5 +1,4 @@
 /*
-  Add in warning time in the array
   Replace the total with sizeOf in the for loop
   Move the period type out of the conditional
   Debug mode
@@ -15,8 +14,6 @@
 ///******************************* WIFI **************************************/
 #define WIFI_SSID "TP-Link_51CA"
 #define WIFI_PASS "password"
-
-
 
 #define DAYLIGHTSAVINGS 1 //Change either to 1 or 0 depending on daylight savings. 0 = fall back (no daylight savings)
 
@@ -44,8 +41,9 @@
 #define HOURS         0
 #define MINUTES       1
 #define DURATION      2
-#define PERIOD        3
-#define SCHEDULE      4
+#define WARNINGTIME   3
+#define PERIOD        4
+#define SCHEDULE      5
 
 
 
@@ -55,42 +53,51 @@ WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
 char periodName[9][10] {"Advisory", "Period 1", "Period 2", "Period 3", "Period 4", "Period 5", "Period 6", "Lunch", "Activity"};
-int scheduleType = REG;
-int nowTime = 460;
 
-int schedule[32][5] {
-  {7, 45, 4, ADV, REG},
-  {7, 52, 56, PR1, REG},
-  {8, 51, 56, PR2, REG},
-  {9, 50, 56, PR3, REG},
-  {10, 51, 56, PR4, REG},
-  {11, 50, 30, LUN, REG},
-  {12, 23, 56, PR5, REG},
-  {13, 22, 56, PR6, REG},
-  {7, 45, 5, ADV, HLF},
-  {7, 53, 42, PR1, HLF},
-  {8, 38, 38, PR2, HLF},
-  {9, 19, 38, PR3, HLF},
-  {10, 0, 38, PR4, HLF},
-  {10, 41, 36, PR5, HLF},
-  {11, 22, 38, PR6, HLF},
-  {7, 45, 5, ADV, AAC},
-  {7, 53, 48, PR1, AAC},
-  {8, 44, 48, PR2, AAC},
-  {9, 35, 44, ACT, AAC},
-  {10, 22, 48, PR3, AAC},
-  {11, 48, 48, PR4, AAC},
-  {12, 6, 30, LUN, AAC},
-  {12, 39, 48, PR5, AAC},
-  {13, 30, 48, PR6, AAC},
-  {7, 45, 30, ADV, EXT},
-  {8, 18, 52, PR1, EXT},
-  {9, 13, 52, PR2, EXT},
-  {10, 8, 52, PR3, EXT},
-  {11, 3, 55, PR4, EXT},
-  {11, 58, 30, LUN, EXT},
-  {12, 31, 52, PR5, EXT},
-  {13, 26, 52, PR6, EXT},
+
+int scheduleType = REG; //<- variable that is changing
+
+
+int nowTime = 460;
+int i;
+
+//{HOURS, MINS, PERIOD LENGTH, WARNING TIME, PERIOD TYPE, SCHEDULE}
+uint8_t schedule[32][6] {
+  {7, 45, 4, 0, ADV, REG},
+  {7, 52, 56, 5, PR1, REG},
+  {8, 51, 56, 5, PR2, REG},
+  {9, 50, 56, 5, PR3, REG},
+  {10, 51, 56, 5, PR4, REG},
+  {11, 50, 30, 5, LUN, REG},
+  {12, 23, 56, 5, PR5, REG},
+  {13, 22, 56, 15, PR6, REG},
+
+  {7, 45, 5, 0, ADV, HLF},
+  {7, 53, 42, 5, PR1, HLF},
+  {8, 38, 38, 5, PR2, HLF},
+  {9, 19, 38, 5, PR3, HLF},
+  {10, 0, 38, 5, PR4, HLF},
+  {10, 41, 36, 5, PR5, HLF},
+  {11, 22, 38, 15, PR6, HLF},
+
+  {7, 45, 5, 0,  ADV, AAC},
+  {7, 53, 48, 5, PR1, AAC},
+  {8, 44, 48, 5, PR2, AAC},
+  {9, 35, 44, 5, ACT, AAC},
+  {10, 22, 48, 5, PR3, AAC},
+  {11, 48, 48, 5, PR4, AAC},
+  {12, 6, 30, 5, LUN, AAC},
+  {12, 39, 48, 5, PR5, AAC},
+  {13, 30, 48, 15, PR6, AAC},
+
+  {7, 45, 30, 0, ADV, EXT},
+  {8, 18, 52, 5, PR1, EXT},
+  {9, 13, 52, 5, PR2, EXT},
+  {10, 8, 52, 5, PR3, EXT},
+  {11, 3, 55, 5, PR4, EXT},
+  {11, 58, 30, 5, LUN, EXT},
+  {12, 31, 52, 5, PR5, EXT},
+  {13, 26, 52, 15, PR6, EXT},
 
 
 
@@ -135,10 +142,12 @@ void loop() {
   //  int secs = timeClient.getSeconds();
   //  int nowTime = hoursMinToMinTotal(hrs, mins);
 
+  uint8_t periodValue = 0;
   nowTime++;
   int hrs = minTotalToHours(nowTime);
   int mins = minTotalToMins(nowTime);
   delay(100);
+
 
 
   Serial.print(hrs);
@@ -148,44 +157,55 @@ void loop() {
   //  Serial.print(secs);
   //  Serial.println(" ");
 
-  for (int i = 0; i < 31; i++) {
-
+  for (i = 0; i < 31; i++) {
 
     if (schedule[i][SCHEDULE] == scheduleType) {
 
       int periodStartTime = hoursMinToMinTotal(schedule[i][HOURS], schedule[i][MINUTES]);
       int periodEndTime = periodStartTime + schedule[i][DURATION];
-      int periodWarningTime = periodEndTime - 5;
-      int nextPeriodTime = hoursMinToMinTotal(schedule[i+1][HOURS], schedule[i+1][MINUTES]); 
-
+      int periodWarningTime = periodEndTime - schedule[i][WARNINGTIME];
+      int nextPeriodTime = hoursMinToMinTotal(schedule[i + 1][HOURS], schedule[i + 1][MINUTES]);
 
       //Class time turn on Green
       if (nowTime > periodStartTime && nowTime <= periodWarningTime) {
+        //Lunch time turn on Red
+        if (schedule[i][PERIOD] == LUN) {
+          Serial.print("LUNCH RED");
+          periodValue = i;
+        }
         //Green light
         Serial.print("GREEN ");
-        Serial.print(periodName[schedule[i][PERIOD]]);
-        Serial.print(" ");
-        Serial.print(i);
-        Serial.print(" ");
+        periodValue = i;
+        
+        //Warning time turn on Yellow
       } else if (nowTime > periodWarningTime && nowTime <= periodEndTime) {
         Serial.print("YELLOW ");
-        Serial.print(periodName[schedule[i][PERIOD]]);
-        Serial.print(" ");
-        Serial.print(i);
-        Serial.print(" ");
-      } else if (nowTime > periodEndTime && nowTime <= nextPeriodTime) { 
+        periodValue = i;
+
+
+
+        //Passing time
+      } else if (nowTime > periodEndTime && nowTime <= nextPeriodTime) {
         Serial.print("RED ");
-        Serial.print(periodName[schedule[i][PERIOD]]);
-        Serial.print(" ");
-        Serial.print(i);
-        Serial.print(" ");
+        periodValue = i;
+
+
+
       }
 
     }
-}
-Serial.println(nowTime);
 
-  if (nowTime > 800) {
+
+  }
+  Serial.print(nowTime);
+  Serial.print(" ");
+  Serial.print(periodName[schedule[periodValue][PERIOD]]);
+  Serial.print(" ");
+  Serial.print(periodValue);
+  Serial.println(" ");
+
+
+  if (nowTime > 900) {
     nowTime = 400;
   }
 
