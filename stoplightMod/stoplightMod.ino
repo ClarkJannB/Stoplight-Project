b1f02933191968133d3705ac0a78f193c49f4361
Clark Jann Barayuga <92810969+ClarkJannB@users.noreply.github.com>
2023-03-24 14:06:54 -0400

    Adafruit IO connect code
    This code now succesfully connects to the Adafruit IO. It doesn't look like it received the value from the dashboard though.

--- stoplightMod/stoplightMod.ino
+++ stoplightMod/stoplightMod.ino
@@ -4,21 +4,50 @@
   Debug mode
   Double check all greater than and less than or equal to signs
 */
-
+// comment out the following lines if you are using fona or ethernet
 #include "AdafruitIO_WiFi.h"
 
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 #include <ESP8266WiFi.h>
 
-///******************************* WIFI **************************************/
+///******************************* WIFI **************************************///
 #define WIFI_SSID "TP-Link_51CA"
 #define WIFI_PASS "password"
+///************************ Adafruit IO Config *******************************///
+// visit io.adafruit.com if you need to create an account,
+// or if you need your Adafruit IO key.
+#define IO_USERNAME "tisnotgonnawork"
+#define IO_KEY "aio_jyyj36jqCPnFHOqxYuhC9To7G9iR" // <- Adafruit might reset this from time to time so make sure to check this is the same
+#if defined(USE_AIRLIFT) || defined(ADAFRUIT_METRO_M4_AIRLIFT_LITE) ||         \
+    defined(ADAFRUIT_PYPORTAL)
+// Configure the pins used for the ESP32 connection
+#if !defined(SPIWIFI_SS) // if the wifi definition isnt in the board variant
+// Don't change the names of these #define's! they match the variant ones
+#define SPIWIFI SPI
+#define SPIWIFI_SS 10 // Chip select pin
+#define NINA_ACK 9    // a.k.a BUSY or READY pin
+#define NINA_RESETN 6 // Reset pin
+#define NINA_GPIO0 -1 // Not connected
+#endif
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS, SPIWIFI_SS,
+                   NINA_ACK, NINA_RESETN, NINA_GPIO0, &SPIWIFI);
+#else
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+#endif
+///***************************** FEED(S) *************************************///
+//trying to use only one variable to control all other vals
+AdafruitIO_Feed *scheduleControl = io.feed("scheduleControl");
+
 
 #define DAYLIGHTSAVINGS 1 //Change either to 1 or 0 depending on daylight savings. 0 = fall back (no daylight savings)
+#define GREEN_PIN 4
+#define YELLOW_PIN 15
+#define RED_PIN 5
+#define LIGHT_ON 1
+#define LIGHT_OFF 0
 
 //Advisory, Lunch, Per 1-6,
-
 //Period defines
 #define ADV      0
 #define PR1      1
@@ -45,8 +74,8 @@
 #define PERIOD        4
 #define SCHEDULE      5
 
-
-
+//time setup
+//offset for time zone
 const long utcOffsetInSeconds = -18000;
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 WiFiUDP ntpUDP;
@@ -54,6 +83,7 @@ NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
 char periodName[9][10] {"Advisory", "Period 1", "Period 2", "Period 3", "Period 4", "Period 5", "Period 6", "Lunch", "Activity"};
 
+//int scheduleControlVal = 0;
 
 int scheduleType = REG; //<- variable that is changing
 
@@ -131,6 +161,33 @@ void setup() {
 
   timeClient.begin();
 
+  // connect to io.adafruit.com
+  Serial.print("Connecting to Adafruit IO");
+  io.connect();
+
+  // set up a message handler for the feeds.
+  // the handleMessage function (defined below)
+  // will be called whenever a message is
+  // received from adafruit io.
+  scheduleControl->onMessage(scheduleControlValue);
+
+
+  // wait for a connection (will print out . until connected to Adafruit IO)
+  // make sure to change IO_USERNAME and IO_KEY  up top
+  while (io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
+
+  //make sure all feeds get their current values right away
+  scheduleControl->get();
+
+
+
 
 }
 
@@ -202,6 +259,8 @@ void loop() {
   Serial.print(periodName[schedule[periodValue][PERIOD]]);
   Serial.print(" ");
   Serial.print(periodValue);
+  Serial.print(" ScheduleType: "); 
+  Serial.print(scheduleType); 
   Serial.println(" ");
 
 
@@ -210,4 +269,26 @@ void loop() {
   }
 
   //  delay(500);
+}
+
+
+// this function is called whenever an 'scheduleControl' feed message
+// is received from Adafruit IO. it was attached to
+// the 'scheduleControl' feed in the setup() function above.
+void scheduleControlValue(AdafruitIO_Data * data) {
+
+  //convert data to integer
+  int scheduleControlRead = data->toInt();
+  Serial.print("received <- ");
+
+  // we can use feedName() in order to find out which feed the message came from.
+  Serial.print(data->feedName());
+  Serial.print(" ");
+
+  // print out the received value
+  Serial.println(scheduleControlRead);
+
+  // set the read value equal to the variable to change which schedule
+  scheduleType = scheduleControlRead;
+
 }
